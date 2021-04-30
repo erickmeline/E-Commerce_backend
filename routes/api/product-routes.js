820cdc9a3acbfc674e374b19338a19dc763369c5
Erick Meline <erickmeline@gmail.com>
2021-04-29 18:35:40 -0700

    add product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category, Tag
+    ]
+  }).then((response) => {
+    res.status(200).json(response);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category, Tag
+    ]
+  })
 });
 
 // create new product
@@ -90,7 +101,13 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((response) => {
+    res.status(200).json(response);
+  });
 });
 
 module.exports = router;
